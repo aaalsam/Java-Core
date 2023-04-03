c1372f5c6fda8c0eb12e8903f9acbf199a523249
AAALSAM <nastya140399@mail.ru>
2023-04-03 21:14:26 +0300

    revision

--- lambda_expressions_and_functional_interfaces/Calculator/src/Calculator.java
+++ lambda_expressions_and_functional_interfaces/Calculator/src/Calculator.java
@@ -6,8 +6,16 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : Integer.max(x, y);
-    //BinaryOperator<Integer> devide = (x, y) -> { if (y != 0) { return x / y; } else { throw new ArithmeticException("Деление на ноль"); }};
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            System.err.println(
+                    "Нельзя делить на ноль!");
+            return Integer.max(x,y);
+        }
+    };
+
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- lambda_expressions_and_functional_interfaces/Worker/src/Main.java
+++ lambda_expressions_and_functional_interfaces/Worker/src/Main.java
@@ -7,31 +7,5 @@ public class Main {
     }
 }
 
-@FunctionalInterface
-interface OnTaskDoneListener {
-    void onDone(String result);
-}
 
-@FunctionalInterface
-interface OnTaskErrorListener {
-    void onError(String error);
-}
-class Worker {
-    private OnTaskDoneListener callback;
-    private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
-        this.callback = callback;
-        this.errorCallback = errorCallback;
-    }
-
-    public void start() {
-        for (int i = 0; i < 100; i++) {
-            if (i==33){
-                errorCallback.onError("Ошибка");
-            } else {
-                callback.onDone("Task " + i + " is done");
-            }
-        }
-    }
-}
